52323a7145241c7e682c3fe238a67ef68e2a3cf1
Apoorva Kumar <apkvandagadde2@gmail.com>
2021-04-08 13:06:33 +0530

    fix the path

--- bot/plugins/utils/image.py
+++ bot/plugins/utils/image.py
@@ -8,16 +8,16 @@ import os
 
 def generate_image(emoji):
     emojifont=os.path.join('')
-    fnt = ImageFont.truetype('G:\\BOT\\airdropbot\\bot\\plugins\\utils\\fonts\\AppleColorEmoji.ttf', size=109, layout_engine=ImageFont.LAYOUT_RAQM)
-    im = Image.open(r'G:\\BOT\\airdropbot\\bot\\plugins\\utils\\image\\f.jpg')
+    fnt = ImageFont.truetype('telegram-airdropbot-bot/bot/plugins/utils/fonts/AppleColorEmoji.ttf', size=109, layout_engine=ImageFont.LAYOUT_RAQM)
+    im = Image.open(r'telegram-airdropbot-bot/bot/plugins/utils/image/f.jpg')
     draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype("G:\\BOT\\airdropbot\\bot\\plugins\\utils\\fonts\\Aaargh.ttf", 28)
+    font = ImageFont.truetype("telegram-airdropbot-bot/bot/plugins/utils/fonts/Aaargh.ttf", 28)
     draw.text((0,0),Config.BOT_NAME,font=font)
     width=[266,450,27,104,358,182]
     height=[176,261,100,168,240,35]
     for (i,w,h) in zip(emoji,width,height):
         draw.text((w,h), i, fill="white", embedded_color=True, font=fnt)
-    im.save(f"G:\\BOT\\airdropbot\\bot\\plugins\\utils\\image\\testemoji.png")
+    im.save(f"telegram-airdropbot-bot/bot/plugins/utils/image/testemoji.png")
 
 
 """def emoji_markup(generated_emoji):
